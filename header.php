bbe7b78c53024dcbeb8da3488ccd3d88b58d4935
Wes Hightower <bundleofstrings@gmail.com>
2016-03-03 15:56:21 -0600

    framework upload
    framework upload

---
+++ header.php
@@ -0,0 +1,5 @@
+<html>
+	<head>
+		<title>Hightower Clinical, LLC</title>
+	</head>
+	<body>
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php include('header.php'); ?>
+		<div id="bodyContent">
+			<h1>Hightower Clinical</h1>
+		</div> <!-- /bodyContent -->
+<?php include('footer.php'); ?>
